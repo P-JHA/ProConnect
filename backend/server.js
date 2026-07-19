5c25d792e6a9b24d301148ed01eb11f07e93e726
Prabhakar Jha <Jhaprabhakar42@gmail.com>
2026-07-19 12:32:12 +0530

    secure MongoDB connection with environment variables

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import moongoose from 'mongoose';
+import postRoutes from './routes/postRoutes.js';
 
 dotenv.config();
 
@@ -11,11 +12,14 @@ app.use(cors());
 app.use(express.json());
 
 const start = async () => {
-    const connectDB = await moongoose.connect("mongodb+srv://jhaprabhakar_db_user:TxTnLKF3N8D0ayUF@connectcluster.tvtssij.mongodb.net/?appName=ConnectCluster");
+    const connectDB = await moongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     app.listen(9080, () => {
         console.log("Server is running on port 9080");
     });
 };
-   
+
 
 start();
